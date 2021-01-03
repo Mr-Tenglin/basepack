0aba08ebcd737f886ec30a91ac75126ea39f327e
Easy Jia Content Management System <31702375+Mr-Tenglin@users.noreply.github.com>
2021-01-04 01:01:03 +0800

    Update Array2Xml.php

--- src/Array2Xml.php
+++ src/Array2Xml.php
@@ -207,6 +207,6 @@ class Array2Xml {
 	}
 
 	protected function removeControlCharacters(string $value): string {
-		return preg_replace("/[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/", "", $value);
+		return preg_replace("/[\\x00-\\x09\\x0B\\x0C\\x0E-\\x1F\\x7F]/", "", $value);
 	}
 }
